b46dfccf21885b40e3a731dbeb04f87bcd806005
777dan <danya.nikitin7@gmail.com>
2023-03-31 09:19:38 +0300

    first commit

---
+++ wp-content/themes/emmet-lite/functions.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * emmet functions and definitions
+ *
+ * @package WordPress
+ * @subpackage emmet
+ * @since emmet 1.0
+ */
+require get_template_directory() . '/inc/theme/init.php';
+
+/**
+ * Note: Do not add any custom code here. Please use a child theme so that your customizations aren't lost during updates.
+ * http://codex.wordpress.org/Child_Themes
+ */
+
+function my_forcelogin_bypass($bypass)
+{
+    if (is_page('register')) {
+        $bypass = true;
+    }
+
+    // Get visited URL without last part
+    $url_path = dirname($_SERVER['REQUEST_URI']);
+
+    // Allow URL
+    if ('/aktivacziya-uchetnoj-zapisi' === $url_path) {
+        $bypass = true;
+    }
+    return $bypass;
+}
+add_filter('v_forcelogin_bypass', 'my_forcelogin_bypass');
